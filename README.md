154cf0dd21b73dd52d28a463a874e85097abda11
Konstantin Krassmann <kkonstantin@gmx.de>
2014-12-30 22:18:29 +0100

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-ngCordova-nfc
-=============
+ngCordova-NFC
+==============
 
-Wrapper for the NFC functionality
+ngCordova Wrapper for cordova-plugin-nfc feature
+
+This is a unofficial ngCordova plugin (just adapts the way the functionality got wrapped).
+
+It will replace the success and fail callbacks with promises.
+
+#Install
+##via bower:
+bower install git://github.com/konstantinkrassmann/ngCordova-nfc --save-dev
+
+#How it works:
+1.Set as dependency of your application
+``` javascript
+angular.module("myApp", ["ngCordova.plugins.nfc"]);
+```
+
+2.Use in componentens:
+``` javascript
+app.controller("StartCtrl", function($cordovaNfc){
+
+  //Because of the problem about the async-ness of the nfc plugin, we need to wait
+  //for it to be ready.
+   $cordovaNfc.then(function(nfcInstance){
+
+        //Use the plugins interface as you go, in a more "angular" way
+        nfcInstance.addNdefListener(function(event){
+            console.log("success");
+        }, function(event){
+            console.log("error");
+        });
+   })
+});
+```
---
+++ nfc.js
@@ -0,0 +1,180 @@
+/*!
+ * ngCordova-nfc *
+ * Copyright 2014 Konstantin Krassmann
+ * @since 30.12.2014
+ * See LICENSE in this repository for license information
+ */
+angular.module("ngCordova.plugins.nfc", [])
+    .factory('$cordovaNfc', ['$q', '$window', function ($q) {
+        var q = $q.defer();
+
+        document.addEventListener('deviceready', function(){
+            q.resolve(
+                {
+                    addNdefListener: function (callback) {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.addNdefListener(callback, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+                        return q.promise;
+                    },
+                    addMimeTypeListener: function (mimeType, callback) {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.addMimeTypeListener(mimeType, callback, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+                        return q.promise;
+                    },
+                    addTagDiscoveredListener: function(callback)
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.addTagDiscoveredListener(callback, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    addNdefFormatableListener: function(callback)
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.addNdefFormatableListener(callback, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    write : function(ndefMessage)
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.write(ndefMessage, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    makeReadOnly: function()
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.makeReadOnly(q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    share : function(ndefMessage)
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.share(ndefMessage, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    unshare: function()
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.unshare(q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    handover: function (uris) {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.handover(uris, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    stopHandover: function()
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.stopHandover(uris, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    erase: function()
+                    {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.erase(q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    removeTagDiscoveredListener: function (callback) {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.removeTagDiscoveredListener(callback, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+                    removeMimeTypeListener: function(mimeType, callback) {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.removeMimeTypeListener(callback, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    },
+
+                    removeNdefListener: function (callback) {
+                        var q = $q.defer();
+
+                        if(typeof nfc != "undefined")
+                        {
+                            nfc.removeNdefListener(callback, q.resolve, q.reject);
+                        }else
+                            q.reject("nfc plugin not defined");
+
+                        return q.promise;
+                    }
+                }
+            )
+        }, false);
+
+        return q.promise;
+    }]);
